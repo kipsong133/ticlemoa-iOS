8f33070d4b5ccfc9d487c096fce0d8946e83b26c
Shin <struct.developer@gmail.com>
2023-01-14 01:04:56 +0900

    feat: 홈 화면 태그 가로스크롤 기능 적용
    - #231
    - 홈 화면 태그 세로스크롤 기능 동작하지 않도록 함
    - 홈 화면 태그 가로스크롤 기능

--- Targets/UserInterface/Sources/Home/HomeViewModel.swift
+++ Targets/UserInterface/Sources/Home/HomeViewModel.swift
@@ -10,36 +10,16 @@ import SwiftUI
 import DomainInterface
 import Combine
 
-struct HomeTag {
-    var tag: Tag
-    var size: CGFloat = 0
-}
-
 final class HomeViewModel: ObservableObject {
-    
     @ObservedObject var modelContainer: ModelContainer
-    
-    @Published var rows: [[Tag]] = []
     @Published var tags: [Tag] = []
-    @Published var tagText = ""
-    @Published var selectedTag: HomeTag? {
+    @Published var selectedTag: Tag? {
         didSet {
-            print("선택한 태그: \(selectedTag!.tag)")
-            if selectedTag?.tag.tagName == "전체" {
-                Task {
-                    do {
-                        try await modelContainer.articleModel.fetch(tagId: nil)
-                    } catch {
-                        print(error.localizedDescription) // TODO: 실패 토스트 메세지 띄우기
-                    }
-                }
-            } else {
-                Task {
-                    do {
-                        try await modelContainer.articleModel.fetch(tagId: selectedTag?.tag.id)
-                    } catch {
-                        print(error.localizedDescription) // TODO: 실패 토스트 메세지 띄우기
-                    }
+            Task {
+                if selectedTag?.tagName == "전체" {
+                    try? await modelContainer.articleModel.fetch(tagId: nil)
+                } else {
+                    try? await modelContainer.articleModel.fetch(tagId: selectedTag?.id)
                 }
             }
         }
@@ -47,94 +27,32 @@ final class HomeViewModel: ObservableObject {
     
     private var anyCancellables: [AnyCancellable] = []
     
-    @Published var homeRows: [[HomeTag]] = []
-    @Published var homeTags: [HomeTag] = []
-    
     init(modelContainer: ModelContainer) {
         self.modelContainer = modelContainer
         self.modelContainer.tagModel.itemsPublisher
             .receive(on: RunLoop.main)
-            .sink { tags in
-                self.homeTags = [HomeTag(tag: WholeTag())]
-                self.homeTags.append(contentsOf: tags.map { HomeTag(tag: $0 )})
-                self.getTags(self.homeTags)
+            .sink { [weak self] tagItems in
+                self?.tags = [WholeTag()] + tagItems
             }
             .store(in: &anyCancellables)
         
-        Task {
-            do {
-                try await modelContainer.tagModel.read()
-                try await modelContainer.articleModel.fetch(tagId: nil)
-                setupTagList()
-            } catch {
-                setupTagList()
-            }
+        Task { [weak self] in
+            try? await modelContainer.tagModel.read()
+            try? await modelContainer.articleModel.fetch(tagId: nil)
+            self?.selectedTag = tags.first
         }
-        
-        func setupTagList() {
-            getTags(self.homeTags)
-            if let firstTag = homeTags.first {
-                selectedTag = firstTag
-            }
-        }
-    }
-    
-    func getTags(_ tagList: [HomeTag]){
-        print(tagList)
-        var tags = tagList
-        var rows: [[HomeTag]] = []
-        var currentRow: [HomeTag] = []
-        
-        var totalWidth: CGFloat = 0
-        
-        let screenWidth = UIScreen.screenWidth - 10
-        //        let tagSpaceing: CGFloat = 14 /*Leading Padding*/ + 30 /*Trailing Padding*/ + 6 + 6 /*Leading & Trailing 6, 6 Spacing*/
-        let tagSpaceing: CGFloat = 16 /*Leading Padding*/ + 16 /*Trailing Padding*/ + 6 + 6 /*Leading & Trailing 6, 6 Spacing*/
-        
-        if !tags.isEmpty {
-            
-            for index in 0..<tags.count {
-                tags[index].size = tags[index].tag.tagName.getSize()
-            }
-            
-            tags.forEach { tag in
-                
-                totalWidth += (tag.size + tagSpaceing)
-                
-                if totalWidth > screenWidth{
-                    totalWidth = (tag.size + tagSpaceing)
-                    rows.append(currentRow)
-                    currentRow.removeAll()
-                    currentRow.append(tag)
-                } else {
-                    currentRow.append(tag)
-                }
-            }
-            
-            if !currentRow.isEmpty {
-                rows.append(currentRow)
-                currentRow.removeAll()
-            }
-            
-            self.homeRows = rows
-        } else {
-            self.homeRows = []
-        }
-        
     }
 }
 
 // MARK: UI Configure
 extension HomeViewModel {
-    
-    func tagButtonColor(by row: HomeTag) -> Color {
-        self.selectedTag?.tag.id == row.tag.id ? Color.white : Color.grey4
-    }
-    
-    func tagBackgroundColor(by row: HomeTag) -> Color {
-        self.selectedTag?.tag.id == row.tag.id ? Color.ticlemoaBlack : Color.grey2
-    }
-    
+//    func tagButtonColor(by row: HomeTag) -> Color {
+//        self.selectedTag?.tag.id == row.tag.id ? Color.white : Color.grey4
+//    }
+//
+//    func tagBackgroundColor(by row: HomeTag) -> Color {
+//        self.selectedTag?.tag.id == row.tag.id ? Color.ticlemoaBlack : Color.grey2
+//    }
 }
 
 private extension HomeViewModel {
